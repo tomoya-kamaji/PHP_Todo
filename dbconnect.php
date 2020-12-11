77101a72ccf47c048ad35c1add639fbd667874c4
tomoya-kamaji <tomoya.kamaji@gmail.com>
2020-12-11 21:45:19 +0900

    チェックアウトテスト

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,9 @@
 <!-- DBの接続 -->
+<!-- 色々追記 -->
 <?php
-try{ 
-$db = new PDO('mysql:dbname=mini_todo;host=localhost:8889;charset=utf8', 'root', 'root');
+try {
+  $db = new PDO('mysql:dbname=mini_todo;host=localhost:8889;charset=utf8', 'root', 'root');
 } catch (PDOException $e) {
-echo 'DB接続エラー！: ' . $e->getMessage();
+  echo 'DB接続エラー！: ' . $e->getMessage();
 }
 ?>
