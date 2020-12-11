aa3b50b780a73f2b9d8f83376d3c1ac4506c602c
tomoya-kamaji <tomoya.kamaji@gmail.com>
2020-12-11 21:42:31 +0900

    コメントの追記

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,6 @@
+<!-- DBの接続 -->
 <?php
-try {
+try{ 
 $db = new PDO('mysql:dbname=mini_todo;host=localhost:8889;charset=utf8', 'root', 'root');
 } catch (PDOException $e) {
 echo 'DB接続エラー！: ' . $e->getMessage();
